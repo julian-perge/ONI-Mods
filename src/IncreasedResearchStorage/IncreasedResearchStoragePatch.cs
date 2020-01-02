924612e8598c5db7b89515fa78871692c6a11275
julian-perge <julianperge@gmail.com>
2020-01-02 12:44:48 -0500

    Forked from Cairath.

---
+++ src/IncreasedResearchStorage/IncreasedResearchStoragePatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Harmony;
+
+namespace IncreasedResearchStorage
+{
+    [HarmonyPatch(typeof(ResearchCenterConfig), "ConfigureBuildingTemplate")]
+    public static class ResearchCenterConfig_ConfigureBuildingTemplate_Patch
+    {
+        public static void Postfix(ref UnityEngine.GameObject go)
+        {
+            go.AddOrGet<Storage>().capacityKg = 3750f;
+            go.AddOrGet<ManualDeliveryKG>().capacity = 3750f;
+            go.AddOrGet<ManualDeliveryKG>().refillMass = 500f;
+            Debug.Log("[IncreasedResearchStorage] loaded...");
+        }
+    }
+    [HarmonyPatch(typeof(AdvancedResearchCenterConfig), "ConfigureBuildingTemplate")]
+    public static class AdvancedResearchCenterConfig_ConfigureBuildingTemplate_Patch
+    {
+        public static void Postfix(ref UnityEngine.GameObject go)
+        {
+            go.AddOrGet<Storage>().capacityKg = 3750f;
+            go.AddOrGet<ManualDeliveryKG>().capacity = 3750f;
+            go.AddOrGet<ManualDeliveryKG>().refillMass = 500f;
+        }
+    }
+    [HarmonyPatch(typeof(CosmicResearchCenterConfig), "ConfigureBuildingTemplate")]
+    public static class CosmicResearchCenterConfig_ConfigureBuildingTemplate_Patch
+    {
+        public static void Postfix(ref UnityEngine.GameObject go)
+        {
+            go.AddOrGet<Storage>().capacityKg = 1500f;
+            go.AddOrGet<ManualDeliveryKG>().capacity = 1500f;
+            go.AddOrGet<ManualDeliveryKG>().refillMass = 15f;
+        }
+    }
+
+}
