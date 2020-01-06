5247ed29d87bd27f6229bc15a51fe949063cb1c1
julian-perge <julianperge@gmail.com>
2020-01-06 01:16:19 -0500

    Simplified patches.

--- src/IncreasedResearchStorage/IncreasedResearchStorage.cs
+++ src/IncreasedResearchStorage/IncreasedResearchStorage.cs
@@ -1,40 +1,41 @@
-﻿using System;
-using Harmony;
-using STRINGS;
+﻿using Harmony;
+using UnityEngine;
+using PeterHan.PLib;
 
 namespace IncreasedResearchStorage
 {
-    [HarmonyPatch(typeof(ResearchCenterConfig), "ConfigureBuildingTemplate")]
-    public static class ResearchCenterConfig_ConfigureBuildingTemplate_Patch
-    {
-        public static void Postfix(ref UnityEngine.GameObject go)
-        {
-            go.AddOrGet<Storage>().capacityKg = 3750f;
-            go.AddOrGet<ManualDeliveryKG>().capacity = 3750f;
-            go.AddOrGet<ManualDeliveryKG>().refillMass = 500f;
-            Debug.Log("[IncreasedResearchStorage] Base Research Center loaded...");
-        }
-    }
-    [HarmonyPatch(typeof(AdvancedResearchCenterConfig), "ConfigureBuildingTemplate")]
-    public static class AdvancedResearchCenterConfig_ConfigureBuildingTemplate_Patch
-    {
-        public static void Postfix(ref UnityEngine.GameObject go)
-        {
-            go.AddOrGet<Storage>().capacityKg = 3750f;
-            go.AddOrGet<ManualDeliveryKG>().capacity = 3750f;
-            go.AddOrGet<ManualDeliveryKG>().refillMass = 500f;
-            Debug.Log("[IncreasedResearchStorage] Advanced Research Center loaded...");
-        }
-    }
-    [HarmonyPatch(typeof(CosmicResearchCenterConfig), "ConfigureBuildingTemplate")]
-    public static class CosmicResearchCenterConfig_ConfigureBuildingTemplate_Patch
-    {
-        public static void Postfix(ref UnityEngine.GameObject go)
-        {
-            go.AddOrGet<Storage>().capacityKg = 1500f;
-            go.AddOrGet<ManualDeliveryKG>().capacity = 1500f;
-            go.AddOrGet<ManualDeliveryKG>().refillMass = 15f;
-            Debug.Log("[IncreasedResearchStorage] Cosmic Research Center loaded...");
-        }
-    }
+
+		[HarmonyPatch(typeof(ResearchCenterConfig), "ConfigureBuildingTemplate")]
+		public static class ResearchCenterConfig_ConfigureBuildingTemplate_Patch
+		{
+				public static void Postfix(ref GameObject go)
+				{
+						go.AddOrGet<Storage>().capacityKg = Options.Opts.BaseCapacity;
+						go.AddOrGet<ManualDeliveryKG>().capacity = Options.Opts.BaseCapacity;
+						go.AddOrGet<ManualDeliveryKG>().refillMass = Options.Opts.BaseRefill;
+						Debug.Log("[IncreasedResearchStorage] Base Research Center loaded...");
+				}
+		}
+		[HarmonyPatch(typeof(AdvancedResearchCenterConfig), "ConfigureBuildingTemplate")]
+		public static class AdvancedResearchCenterConfig_ConfigureBuildingTemplate_Patch
+		{
+				public static void Postfix(ref GameObject go)
+				{
+						go.AddOrGet<Storage>().capacityKg = Options.Opts.AdvCapacity;
+						go.AddOrGet<ManualDeliveryKG>().capacity = Options.Opts.AdvCapacity;
+						go.AddOrGet<ManualDeliveryKG>().refillMass = Options.Opts.AdvRefill;
+						Debug.Log("[IncreasedResearchStorage] Advanced Research Center loaded...");
+				}
+		}
+		[HarmonyPatch(typeof(CosmicResearchCenterConfig), "ConfigureBuildingTemplate")]
+		public static class CosmicResearchCenterConfig_ConfigureBuildingTemplate_Patch
+		{
+				public static void Postfix(ref GameObject go)
+				{
+						go.AddOrGet<Storage>().capacityKg = Options.Opts.CosCapacity;
+						go.AddOrGet<ManualDeliveryKG>().capacity = Options.Opts.CosCapacity;
+						go.AddOrGet<ManualDeliveryKG>().refillMass = Options.Opts.CosRefill;
+						Debug.Log("[IncreasedResearchStorage] Cosmic Research Center loaded...");
+				}
+		}
 }
