2fa33410659176e0886b66707680781dc2194270
julian-perge <julianperge@gmail.com>
2020-01-06 01:15:26 -0500

    Created Options class for overall ease of use. From Peter Han, AzeTheGreat, Cairath.

---
+++ src/IncreasedResearchStorage/Util/Options.cs
@@ -0,0 +1,57 @@
+﻿using AzeLib;
+using Newtonsoft.Json;
+using PeterHan.PLib;
+using UnityEngine;
+
+namespace IncreasedResearchStorage
+{
+		[JsonObject(MemberSerialization.OptIn)]
+		[PeterHan.PLib.Options.RestartRequired]
+		public class Options : BaseOptions<Options>
+		{
+				[Option("Base Station Capacity", "How many kg of dirt the station can store. Default is 750kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float BaseCapacity { get; set; }
+
+				[Option("Base Station Refill Amount", "What capacity Dupes will refill at. Default is 150kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float BaseRefill { get; set; }
+
+				[Option("Advanced Station Capacity", "How many kg of water the station can store. Default is 750kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float AdvCapacity { get; set; }
+
+				[Option("Advanced Station Refill Amount", "What capacity Dupes will refill at. Default is 150kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float AdvRefill { get; set; }
+
+				[Option("Cosmic Station Capacity", "How many kg of data banks the station can store. Default is 300kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float CosCapacity { get; set; }
+
+				[Option("Cosmic Station Refill Amount", "What capacity Dupes will refill at. Default is 3kg. Max is 999,999kg")]
+				[Limit(1, 999999)]
+				[JsonProperty]
+				public float CosRefill { get; set; }
+
+				public Options()
+				{
+						BaseCapacity = 750f;
+						BaseRefill = 150f;
+						AdvCapacity = 750f;
+						AdvRefill = 150f;
+						CosCapacity = 300f;
+						CosRefill = 3f;
+				}
+
+				public static void OnLoad()
+				{
+						Load();
+				}
+		}
+}
