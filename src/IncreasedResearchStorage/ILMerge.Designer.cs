f2c8ca69446392dc2ca196ea7cae3403902a6b21
julian-perge <julianperge@gmail.com>
2020-01-06 02:49:01 -0500

    Updated project files.

--- src/IncreasedResearchStorage/ILMerge.Designer.cs
+++ src/IncreasedResearchStorage/ILMerge.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace IncreasedResearchStorage {
+namespace JulianPerge.IncreasedResearchStorage {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
--- src/IncreasedResearchStorage/Properties/AssemblyInfo.cs
+++ src/IncreasedResearchStorage/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rounded Research Points")]
-[assembly: AssemblyDescription("Rounds the research points generated to 2 decimals. Fixes rounding errors, especially with data banks.")]
+[assembly: AssemblyTitle("Increased Research Storage")]
+[assembly: AssemblyDescription("Increases the storage and refill amounts for the base, super and cosmic computers.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Julian Perge OxygenNotIncluded Mods")]
@@ -14,23 +14,20 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("47241581-b8c4-4d3a-8fa8-25a9cec422dd")]
+[assembly: Guid("61577753-976a-4640-8f66-8e99e30ad671")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
