a71b7d5f2d2e1c3b3e6573e378185d9f100b3164
julian-perge <julianperge@gmail.com>
2020-01-06 01:10:40 -0500

    Renamed Research Points

---
+++ src/RoundedResearchPoints/RoundedResearchPoints.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using System;
+
+namespace RoundedResearchPoints
+{
+		[HarmonyPatch(typeof(ResearchCenter), "ConvertMassToResearchPoints")]
+		public static class ResearchCenter_ConvertMassToResearchPoints_Patch
+		{
+				public static void Prefix(ref float mass_consumed)
+				{
+						// Round to 2 decimals.
+						mass_consumed = (float)Math.Round(mass_consumed, 2);
+				}
+		}
+}
